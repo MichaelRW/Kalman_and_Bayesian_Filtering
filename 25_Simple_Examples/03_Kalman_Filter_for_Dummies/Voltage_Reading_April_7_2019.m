3312f744bc7e99e12b234cda9d7c8c122b93fa7b
Michael Wirtzfeld <michael.wirtzfeld@gmail.com>
2019-04-07 12:13:59 -0500

    Addition of "Voltage_Reading_April_7_2019.m".  Sunday, April 7, 2019 at 12:13.

--- 25_Simple_Examples/03_Kalman_Filter_for_Dummies/Voltage_Reading_April_7_2019.m
+++ 25_Simple_Examples/03_Kalman_Filter_for_Dummies/Voltage_Reading_April_7_2019.m
@@ -9,33 +9,34 @@ set(0, 'DefaultFigureWindowStyle', 'docked');
 
 %% System Variables
 
-X=5; % Initial position state estimate.
-timeStep_seconds=1;  % State update time interval.
-velocity_m_per_s=5; %speed/control = 2m/s
+X=0; % Initial position state estimate.
+timeStep_ms=1;  % State update time interval.
 
 rng(0);  % Fix pseudo-random number generator to produce identical values.
 
 
 %% Kalman Filter Variables
 
-% Initial State Values (1D System)
-x=50;  % Initial position of vehicle.
-P=100;  % Variance estimate of initial position.
+x=0;  % Initial position of vehicle.
+P=1;  % Variance estimate of initial position.
 
 % Time Update Parameters
 A=1;  % State transition matrix.
-B=1;  % Control input matrix.
-Q=2;  % Process noise variance.
+B=0;  % Control input matrix.
+Q=0.01;  % Process noise variance.
 
 % Measurement Update Parameters
 H=1;  % Measurement matrix.
-R=1e2;  % Measurement noise variance.
+R=0.1;  % Measurement noise variance.
+
+% measurements=[ 0.39 0.50 0.48 0.29 0.25 0.32 0.34 0.48 0.41 0.45];
+measurements=sqrt(0.5)*randn(1, 1e2)+0.5;
 
 
 
 %% Initialize Processing Variables
 
-simulationSize=50;
+simulationSize=numel(measurements);
     XX=zeros(1, simulationSize);
     tt=zeros(1, simulationSize);
     xx_predicted=zeros(1, simulationSize);
@@ -50,17 +51,10 @@ simulationSize=50;
 
 % h1=figure('Name', 'Time Update Gaussian');
 
-for t=1:1:simulationSize
+for t=1:1:numel(measurements);
     
-    n=sqrt(Q) * randn();  % State prediction noise (zero mean Gaussian distribution).
-        X=X + velocity_m_per_s*timeStep_seconds + n;  % Predict next state.
-        
-    v=sqrt(R) * randn();  % State measurement noise (zero mean Gaussian distribution).
-        y=H*X + v;  % Measurement of state.
-    
-        
    % Time Update (Prediction) -  Prior 
-    x_predicted=A*x + B*velocity_m_per_s;  % Predicting the new state (mean).
+    x_predicted=A*x;  % Predicting the new state (mean).
         P=A*P*A.' + Q;  % Update the uncertainty of the new state.
     %
 %     figure(h1); hold on; plot([0:0.1:200], normpdf([0:0.1:200], x_predicted, sqrt(P))); grid on; shg;
@@ -68,7 +62,7 @@ for t=1:1:simulationSize
     
     % Measurement Update (Correction) -  Posterior
     K=(H*P*H.'+R)\P*H.';  % Compute Kalman gain.    
-        x=x_predicted+K*(y-H*x_predicted);  % Update the estimate via y.    
+        x=x_predicted+K*(measurements(t)-H*x_predicted);  % Update the estimate via y.    
         P=(1-K*H)*P;
 	%
 %     figure(h1); hold on; plot([0:0.1:200], normpdf([0:0.1:200], x, sqrt(P)), 'r'); shg; pause(1);    
@@ -76,7 +70,7 @@ for t=1:1:simulationSize
 
 	% Store data for plotting prior state values.
     xx_predicted(t)=x_predicted;
-    xx(t)=x; PP(t)=P; XX(t)=X; tt(t)=t; yy(t)=y; kk(t)=K;
+    xx(t)=x; PP(t)=P; XX(t)=X; tt(t)=t; kk(t)=K;
     
 end
 
@@ -85,19 +79,17 @@ end
 %% Display Results
 
 figure(); ...
-    plot(tt, XX, 'k', 'Marker', 'o'); hold on;
-    plot(tt, xx, 'r', 'Marker', '^');
-    plot(tt, xx_predicted);
-    plot(tt, yy, 'b', 'Marker', 's');
-    legend('Ground Truth', 'Corrected State (a Posterior)', 'Predicted State (a Prior)', 'Measurements', 'Location', 'SouthEast');
-    xlabel('Time (seconds)'); ylabel('Position'); title('1D Kalman Filter');
+    plot(tt, xx, 'k', 'Marker', 'o'); hold on;
+    plot(tt, xx_predicted, 'r', 'Marker', '^');
+    plot(tt, measurements, 'b', 'Marker', 's');
+    plot(tt, 0.5*ones(1, numel(tt)), 'm', 'Marker', 'v');
+    legend('Predicted (a Prior)', 'Measurement Update (a Posterior)', 'Measurements', 'Mean Voltage', 'Location', 'SouthEast');
+    xlabel('Time (seconds)'); ylabel('Volts'); title('1D Kalman Filter');
     grid on; shg;
     
 figure(); plot(tt, PP); grid on; shg;
 
 figure(); plot(tt, kk); grid on; shg;
-
-figure(); plot(abs(XX-xx)); hold on; plot(abs(XX-xx_predicted), 'r'); grid on; shg; legend('a Posterior', 'a Prior');
 
     
     
@@ -108,6 +100,8 @@ fprintf(1, '\n*** Processing Complete ***\n');
 
 
 %% Reference(s)
+
+% http://bilgin.esme.org/BitsAndBytes/KalmanFilterforDummies#
 
 % https://www.mathworks.com/examples/statistics/mw/stats-ex15608473-compute-and-plot-the-normal-distribution-pdf
 
